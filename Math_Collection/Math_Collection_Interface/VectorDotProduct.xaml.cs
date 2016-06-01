743cd47d6ab4f174d1d51f61dd4bcbb12b32b730
Felix-Seip <felixseip01@gmail.com>
2016-06-01 20:37:46 +0200

    Vector Textboxeingabefelder definiert

--- Math_Collection/Math_Collection_Interface/VectorDotProduct.xaml.cs
+++ Math_Collection/Math_Collection_Interface/VectorDotProduct.xaml.cs
@@ -45,12 +45,27 @@ namespace Math_Collection_Interface
 
         private void FirstVectorSizeComboBoxSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            
+            AddTextBoxes(0, 5);
         }
 
         private void SecondVectorSizeComboBoxSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            AddTextBoxes(3, 2);
+        }
 
+        private void AddTextBoxes(int column, int textboxCount)
+        {
+            for (int i = 0; i < textboxCount; i++)
+            {
+                TextBox valueInputTextBox = new TextBox();
+                valueInputTextBox.SetValue(Grid.RowProperty, i);
+                valueInputTextBox.SetValue(Grid.RowSpanProperty, 1);
+
+                valueInputTextBox.SetValue(Grid.ColumnProperty, column);
+                valueInputTextBox.SetValue(Grid.ColumnSpanProperty, 1);
+                valueInputTextBox.Margin = new Thickness(10);
+                textBoxGrid.Children.Add(valueInputTextBox);
+            }
         }
     }
 }
