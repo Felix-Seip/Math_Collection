702c9822dfee5d88525354f22ec6daf9a7c1687d
ikke09 <development@fam-bechtluft.de>
2016-06-21 15:48:19 +0200

    + ValidTextBox User Control

---
+++ Math_Collection/Math_Collection_Interface/ExtendedTextBox.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Math_Collection_Interface
+{
+    /// <summary>
+    /// Interaction logic for ExtendedTextBox.xaml
+    /// </summary>
+    public partial class ExtendedTextBox : UserControl
+    {
+        private bool _isValid;
+        public bool IsValid
+        {
+            get { return _isValid; }
+            set { _isValid = value; }
+        }
+
+        public ExtendedTextBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Math_Collection/Math_Collection_Interface/ValidTextBox.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Math_Collection_Interface
+{
+    /// <summary>
+    /// Interaction logic for ValidTextBox.xaml
+    /// </summary>
+    [Designer("System.Windows.Forms.Design.ParentControlDesigner, System.Design", typeof(System.ComponentModel.Design.IDesigner))]
+    public partial class ValidTextBox : UserControl
+    {
+        private bool _isValid;
+        public bool IsValid
+        {
+            get { return _isValid; }
+            set
+            {
+                _isValid = value;
+
+                txtBox.BorderThickness = new Thickness(2);
+                if (_isValid)
+                    txtBox.BorderBrush = Brushes.Green;
+                else
+                    txtBox.BorderBrush = Brushes.Red;
+            }
+        }
+
+        private string _header;
+        public string Header
+        {
+            get { return _header; }
+            set
+            {
+                if(String.IsNullOrEmpty(_header))
+                    _header = value;
+
+                lblHeader.Content = _header;
+            }
+        }
+        public ValidTextBox(string header = "")
+        {
+            InitializeComponent();
+            IsValid = true;
+            Header = header;
+        }
+
+        private void txtBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            IsValid = ValueValidator.isValidNumber(txtBox.Text);
+        }
+    }
+}
