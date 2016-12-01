79292a8fa31e101760dee9dceee4a6e4f78bb19b
ikke09 <development@fam-bechtluft.de>
2016-12-01 17:58:38 +0100

    Added UnitTest
    UnitTest Projekt

---
+++ Math_Collection/Math_Collection_UnitTest/Analysis_Test.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Analysis = Math_Collection.Analysis.AnalysisBase;
+
+namespace Math_Collection_UnitTest
+{
+    [TestClass]
+    public class Analysis_Test
+    {
+        private static List<double> values;
+
+        [ClassInitialize]
+        public static void initClass(TestContext context)
+        {
+            values = new List<double>();
+        }
+
+        [TestInitialize]
+        public void initTest()
+        {
+            values.Clear();   
+        }
+
+        [TestMethod]
+        public void DerivationApproximation_Test()
+        {
+            double actual = 0.0;
+            double expected = 0.0;
+            double h = 0.001;
+
+            //not enough values
+            values.Add(3);
+            expected = double.NaN;
+            actual = Analysis.Derivation_Approximation(values.ToArray(), 1, h);
+            Assert.AreEqual(expected, actual);
+            values.Clear();
+
+            //Function f(x) = 3
+            for (int i = 0; i < 10; i++)
+                values.Add(3.0);
+
+            expected = 0.0;
+            actual = Analysis.Derivation_Approximation(values.ToArray(), 2, h);
+            Assert.AreEqual(expected, actual);
+            values.Clear();
+
+            values.Add(0);
+            values.Add(1);
+            values.Add(4);
+            expected = 2.0;
+            actual = Analysis.Derivation_Approximation(values.ToArray(), 1, h, true);
+            Assert.AreEqual(expected, actual);
+            
+        }
+    }
+}
---
+++ Math_Collection/Math_Collection_UnitTest/LinearAlgebra_Test.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Math_Collection;
+using Math_Collection.LinearAlgebra.Matrices;
+using Math_Collection.LinearAlgebra;
+
+namespace Math_Collection_UnitTest
+{
+    [TestClass]
+    public class LinearAlgebra_Test
+    {
+        [TestMethod]
+        public void SwitchRows_Test()
+        {
+            double[,] elements = new double[3, 3] { { 1, 1, 1 }, { 2, 2, 2 }, { 3, 3, 3 } };
+            Matrix m = new Matrix(elements);
+
+            Matrix actual = LinearAlgebraOperations.SwitchRows(m, 2, 1);
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void LR_Partition_Test()
+        {
+            Matrix input = new Matrix(new double[3, 3] { { 1, 2, 3 }, { 1, 1, 1 }, { 3, 3, 1 } });
+            Matrix l = new Matrix();
+            Matrix r = new Matrix();
+
+            LinearAlgebraOperations.LR_Partition(input, out l, out r);
+
+            int i = 0;
+
+        }
+
+
+    }
+}
