2b91535753b5cd379e3fcd1f1f7ef72ce49fbb4b
ikke09 <development@fam-bechtluft.de>
2016-12-03 22:35:10 +0100

    H-Method
    + implemented h-Method

--- Math_Collection/Math_Collection/AnalysisBase.cs
+++ Math_Collection/Math_Collection/AnalysisBase.cs
@@ -1,4 +1,5 @@
 ﻿using Math_Collection.LinearAlgebra.Vectors;
+using System;
 
 namespace Math_Collection.Analysis
 {
@@ -37,6 +38,28 @@ namespace Math_Collection.Analysis
                     derivedExpression += derivation[i] + "x^" + exponents[i];
             }
             return derivedExpression;
+        }
+
+        /// <summary>
+        /// Calculated the approximated derivation at a point with the h-method
+        /// </summary>
+        /// <param name="functionValues"></param>
+        /// <param name="x">x value of the function</param>
+        /// <param name="h"></param>
+        /// <param name="arimeticMiddle">use arimeticMiddle?</param>
+        /// <returns>the approximated value of the derivation or NaN if it fails</returns>
+        public static double Derivation_Approximation(double[] functionValues, int x, double h, bool arimeticMiddle = false)
+        {
+            if (functionValues.Length < 3 || x < 1)
+                return double.NaN;
+
+            if (arimeticMiddle)
+            {
+                return Math.Round((functionValues[x + 1] - functionValues[x - 1]) / (2 * h),4);
+            }else
+            {
+                return Math.Round(((functionValues[x + 1] - functionValues[x]) / h), 4);
+            }
         }
     }
 }
