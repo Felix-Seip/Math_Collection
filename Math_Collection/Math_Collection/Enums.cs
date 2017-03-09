a31afa17f44756c689c312b698acc9c2f4ac68a9
Felix-Seip <felixseip01@gmail.com>
2017-03-09 11:14:15 +0100

    Math_Collection Umsortiert
    Matrix classes moved into the folder LinearAlgebra\Matrices.
    Added Enum class and moved interval enum
    Renamed Intervall.cs to Interval.cs

---
+++ Math_Collection/Math_Collection/Enums.cs
@@ -0,0 +1,26 @@
+﻿namespace Math_Collection
+{
+    public class Enums
+    {
+        public enum eIntervalFeature
+        {
+            /// <summary>
+            /// { x | a < x < b }
+            /// </summary>
+            eOpen = 0,
+            /// <summary>
+            /// { x | a <= x <= b }
+            /// </summary>
+            eClosed,
+            /// <summary>
+            /// { x | a < x <= b }
+            /// </summary>
+            eLeftOpenRightClosed,
+            /// <summary>
+            /// { x | a <= x < b }
+            /// </summary>
+            eLeftClosedRightOpen
+        }
+
+    }
+}
---
+++ Math_Collection/Math_Collection/Interval.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Math_Collection.Basics
+{
+    public class Interval
+    {
+        /// <summary>
+        /// Represents the lower boundary of the intervall
+        /// </summary>
+        public double MinValue { get; set; }
+        /// <summary>
+        /// Represents the higher boundary of the invervall
+        /// </summary>
+        public double MaxValue { get; set; }
+        /// <summary>
+        /// Represents the option of the intervall
+        /// </summary>
+        public Enums.eIntervalFeature feature { get; private set; }
+
+
+        public Interval(double a, double b, Enums.eIntervalFeature f = Enums.eIntervalFeature.eClosed)
+        {
+            MinValue = Math.Min(a,b);
+            MaxValue = Math.Max(a,b);
+            feature = f;
+        }
+    }
+}
