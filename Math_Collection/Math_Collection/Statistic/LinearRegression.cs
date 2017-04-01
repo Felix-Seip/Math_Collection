2c4d177aec3f6ccc35e3007585f0276942c93e62
Felix-Seip <felixseip01@gmail.com>
2017-04-01 23:33:37 +0200

    Added linear regression

---
+++ Math_Collection/Math_Collection/Statistic/LinearRegression.cs
@@ -0,0 +1,108 @@
+﻿using Math_Collection.LinearAlgebra.Matrices;
+using System.Collections.Generic;
+using System.Drawing;
+using System;
+using Math_Collection.LinearAlgebra.Vectors;
+using RuntimeFunctionParser;
+
+namespace Math_Collection.Statistic
+{
+    public static class LinearRegression
+    {
+        public static Function CreateRegression(Matrix regressionMatrix, Vector regressionVector, int numberOfVariables)
+        {
+            Parser parser = new Parser();
+
+            LGS.LGS lgs = new LGS.LGS(regressionMatrix, regressionVector);
+            string function = CreateRegressionFunction(lgs.Solve(Enums.ESolveAlgorithm.eGauß), numberOfVariables);
+            return parser.ParseFunction(function);
+        }
+
+        private static string CreateRegressionFunction(Vector lgsResult, int numberOfVariables)
+        {
+            string function = "";
+            int varCount = numberOfVariables;
+            for (int i = 0; i < varCount; i++)
+            {
+                if (numberOfVariables != 1)
+                {
+                    function += string.Format(lgsResult[i] + "*x^{0}", numberOfVariables - 1) + "+";
+                    numberOfVariables--;
+                }
+                else
+                {
+                    function += lgsResult[i];
+                }
+            }
+            return function;
+        }
+
+        public static Vector CreateRegressionVector(List<Point> points, int power)
+        {
+            Vector regressionVector = new Vector(new double[power]);
+
+            for (int i = 0; i < power; i++)
+            {
+                regressionVector[i] = SumXYValues(points, power - (i + 1));
+            }
+
+            return regressionVector;
+        }
+
+        public static Matrix CreateRegressionMatrix(List<Point> points, int matrixDimension)
+        {
+            Matrix regressionSumMatrix = new Matrix(new double[matrixDimension, matrixDimension]);
+
+            if (matrixDimension != 2 && matrixDimension % 2 == 0)
+            {
+                matrixDimension += 2;
+            }
+            else if (matrixDimension != 2 && matrixDimension % 2 != 0)
+            {
+                matrixDimension += 1;
+            }
+
+            for (int j = 0; j < regressionSumMatrix.RowCount; j++)
+            {
+                for (int i = 0; i < regressionSumMatrix.ColumnCount; i++)
+                {
+                    double sum = SumXValuesForMatrix(points, matrixDimension - i);
+
+                    regressionSumMatrix[j, i] = sum;
+                }
+                matrixDimension--;
+            }
+            return regressionSumMatrix;
+        }
+
+        private static double SumXValuesForMatrix(List<Point> points, int power)
+        {
+            double sum = 0;
+            for (int i = 0; i < points.Count; i++)
+            {
+                sum += Math.Pow(points[i].X, power);
+            }
+            return sum;
+        }
+
+        private static double SumXYValues(List<Point> points, int power)
+        {
+            double sum = 0;
+            if (power != 0)
+            {
+                for (int i = 0; i < points.Count; i++)
+                {
+                    sum += Math.Pow(points[i].X, power) * points[i].Y;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < points.Count; i++)
+                {
+                    sum += points[i].Y;
+                }
+            }
+            return sum;
+        }
+    }
+}
