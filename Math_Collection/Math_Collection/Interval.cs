0a36cc804b09b1ff1131e1365775e3996ceeb65d
ikke09 <development@fam-bechtluft.de>
2017-05-02 06:50:32 +0200

    Extended Interval
    + added a interval step property
    + interval feature is used in the getter from min/max value

--- Math_Collection/Math_Collection/Interval.cs
+++ Math_Collection/Math_Collection/Interval.cs
@@ -2,20 +2,55 @@
 
 namespace Math_Collection.Basics
 {
-    public class Interval
-    {
-        /// <summary>
-        /// Represents the lower boundary of the intervall
-        /// </summary>
-        public double MinValue { get; set; }
-        /// <summary>
-        /// Represents the higher boundary of the invervall
-        /// </summary>
-        public double MaxValue { get; set; }
-        /// <summary>
-        /// Represents the option of the intervall
-        /// </summary>
-        public Enums.eIntervalFeature feature { get; private set; }
+	public class Interval
+	{
+		private double _minValue;
+		/// <summary>
+		/// Represents the lower boundary of the intervall
+		/// </summary>
+		public double MinValue
+		{
+			get
+			{
+				if (Feature == Enums.eIntervalFeature.eOpen || Feature == Enums.eIntervalFeature.eLeftOpenRightClosed)
+					return _minValue + Step;
+
+				return _minValue;
+			}
+			set
+			{
+				_minValue = value;
+			}
+		}
+
+		private double _maxValue;
+		/// <summary>
+		/// Represents the higher boundary of the invervall
+		/// </summary>
+		public double MaxValue
+		{
+			get
+			{
+				if (Feature == Enums.eIntervalFeature.eOpen || Feature == Enums.eIntervalFeature.eLeftClosedRightOpen)
+					return _maxValue - Step;
+
+				return _maxValue;
+			}
+			set
+			{
+				_maxValue = value;
+			}
+		}
+
+		/// <summary>
+		/// Represents the increment value
+		/// </summary>
+		public double Step { get; private set; }
+
+		/// <summary>
+		/// Represents the option of the intervall
+		/// </summary>
+		public Enums.eIntervalFeature Feature { get; private set; }
 
 		public double Range
 		{
@@ -25,12 +60,18 @@ namespace Math_Collection.Basics
 			}
 		}
 
+		public Interval() : this(0.0, 0.0, 1.0, Enums.eIntervalFeature.eClosed)
+		{ }
 
-        public Interval(double a, double b, Enums.eIntervalFeature f = Enums.eIntervalFeature.eClosed)
-        {
-            MinValue = Math.Min(a,b);
-            MaxValue = Math.Max(a,b);
-            feature = f;
-        }
-    }
+		public Interval(double min, double max, Enums.eIntervalFeature f = Enums.eIntervalFeature.eClosed) : this(min, max, 1.0, f)
+		{ }
+
+		public Interval(double min, double max, double step, Enums.eIntervalFeature feature = Enums.eIntervalFeature.eClosed)
+		{
+			MinValue = Math.Min(min, max);
+			MaxValue = Math.Max(min, max);
+			Step = step;
+			Feature = feature;
+		}
+	}
 }
